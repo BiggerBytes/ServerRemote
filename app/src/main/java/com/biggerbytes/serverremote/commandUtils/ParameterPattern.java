11a5fe35576ff3fb703d58ba791efebe39f70447
shach <shacham6@gmail.com>
2016-04-26 12:39:23 +0300

    Fixed ParameterPattern.java

--- app/src/main/java/com/biggerbytes/serverremote/commandUtils/ParameterPattern.java
+++ app/src/main/java/com/biggerbytes/serverremote/commandUtils/ParameterPattern.java
@@ -8,30 +8,48 @@ package com.biggerbytes.serverremote.commandUtils;
  */
 public enum ParameterPattern {
 
-    SET_INFO_THREAD_STATE(0x60, 1),
-    SET_REFRESH_RATE(0x61, 1),
-    READ_INFO_NOW(0x62, 0),
-    ADD_CANCEL_DUMMY(0x63, 1, 2, 24),
-    ADD_SUB_TEACHER(0x64, 1, 2, 24),
-    REMOVE_ALL_DUMMIES_FROM_ID(0x65, 1);
+    SET_INFO_THREAD_STATE(0x60,
+            new int[]{1},
+            new Class[]{Integer.class}),
+
+    SET_REFRESH_RATE(0x61,
+            new int[]{1},
+            new Class[]{Integer.class}),
+
+    READ_INFO_NOW(0x62,
+            new int[]{0},
+            new Class[]{Void.class}),
+
+    ADD_CANCEL_DUMMY(0x63,
+            new int[]{1, 2, 10, 32},
+            new Class[]{Integer.class, String.class, String.class, String.class}),
+
+    ADD_SUB_TEACHER(0x64,
+            new int[]{1, 2, 10, 32},
+            new Class[]{Integer.class, String.class, String.class, String.class}),
+
+    REMOVE_ALL_DUMMIES_FROM_ID(0x65,
+            new int[]{1},
+            new Class[]{Integer.class});
 
 
     private byte flag;
     private byte[][] parameters;
+    private Class[] parameterTypes;
 
-    ParameterPattern(int flag, int... sizes) {
+    ParameterPattern(int flag, int[] sizes, Class[] classes) {
         this.flag = (byte) flag;
 
         //  Building the parameters pattern:
         parameters = new byte[sizes.length][];
         for (int i = 0 ; i < sizes.length; i++ ) parameters[i] = new byte[sizes[i]];
 
-
+        this.parameterTypes = classes;
 
     }
 
-    static byte[][] findPatternsWithFlag(byte flag) {
-        for (ParameterPattern pp : values()) if (pp.flag == flag) return pp.parameters;
+    static ParameterPattern findPatternsWithFlag(byte flag) {
+        for (ParameterPattern pp : values()) if (pp.flag == flag) return pp;
         return null;
     }
 
@@ -41,5 +59,15 @@ public enum ParameterPattern {
         return sum;
     }
 
+    public byte getFlag() {
+        return flag;
+    }
 
+    public byte[][] getParameters() {
+        return parameters;
+    }
+
+    public Class[] getParameterTypes() {
+        return parameterTypes;
+    }
 }
